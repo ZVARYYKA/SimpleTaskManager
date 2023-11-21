ed725cf02940c73b8bf6eea5a0b2c1f9f538f7af
ZVARYYKA <68435439+ZVARYYKA@users.noreply.github.com>
2023-11-21 19:37:10 +0300

    add more stuff

--- src/main/java/com/zvaryyka/simpletaskmanager/controllers/TaskController.java
+++ src/main/java/com/zvaryyka/simpletaskmanager/controllers/TaskController.java
@@ -1,21 +1,27 @@
 package com.zvaryyka.simpletaskmanager.controllers;
 
 import com.zvaryyka.simpletaskmanager.models.Person;
+import com.zvaryyka.simpletaskmanager.models.Task;
 import com.zvaryyka.simpletaskmanager.services.PersonDetailsService;
+import com.zvaryyka.simpletaskmanager.services.TaskService;
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.*;
 
 import java.security.Principal;
 
 @Controller
 public class TaskController {
     private final PersonDetailsService personDetailsService;
+    private final TaskService taskService;
 
     @Autowired
-    public TaskController(PersonDetailsService personDetailsService) {
+    public TaskController(PersonDetailsService personDetailsService, TaskService taskService) {
         this.personDetailsService = personDetailsService;
+        this.taskService = taskService;
     }
 
     @GetMapping("/task")
@@ -23,8 +29,35 @@ public class TaskController {
         Person person = personDetailsService.findByLogin(principal.getName()).stream().findFirst().orElse(null);
         person.getTasks();
         model.addAttribute("person",person);
+        model.addAttribute("taskForAdd",new Task());
+        model.addAttribute("editTask",new Task());
         System.out.println(principal.getName());
         return "task/tasks";
+    }
+    @PostMapping("/createTask")
+    public String createNewTask(@ModelAttribute("taskForAdd")  Task taskForAdd,Principal principal, BindingResult bindingResult) {
+        if(bindingResult.hasErrors())
+            return "task/tasks";
+        Person person = personDetailsService.findByLogin(principal.getName()).stream().findFirst().orElse(null);
+        taskForAdd.setUser(person);
+        taskService.save(taskForAdd);
+        return "redirect:/task";
+    }
+    @PostMapping("/editTask/{id}")
+    public String update(@ModelAttribute("editTask")  Task editTask,Principal principal, BindingResult bindingResult,
+                         @PathVariable("id") int id) {
+        if (bindingResult.hasErrors())
+            return "task/tasks";
+        Person person = personDetailsService.findByLogin(principal.getName()).stream().findFirst().orElse(null);
+        editTask.setUser(person);
+        taskService.update(id, editTask);
+        return "redirect:/task";
+    }
+
+    @PostMapping("/deleteTask/{id}")
+    public String delete(@PathVariable("id") int id) {
+        taskService.delete(id);
+        return "redirect:/task";
     }
 //TODO Создание контроллера для обработки задач, связанные с задачником
 }
